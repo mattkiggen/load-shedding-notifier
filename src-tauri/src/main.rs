4ba64dc6f54ec45ff3f74f04ad85f2ecd1fccc3d
Matt Kiggen <28730291+mattkiggen@users.noreply.github.com>
2023-03-26 10:15:02 +0200

    Add example system tray

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use notify_rust::Notification;
+use tauri::{CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem, SystemTray};
 
 #[tauri::command]
 fn notify() -> bool {
@@ -19,7 +20,23 @@ fn notify() -> bool {
 }
 
 fn main() {
+    let show = CustomMenuItem::new("show".to_string(), "Show");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(show)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    let tray = SystemTray::new().with_menu(tray_menu);
+
     tauri::Builder::default()
+        .system_tray(tray)
+        .on_window_event(|event| match event.event() {
+          tauri::WindowEvent::CloseRequested { api, .. } => {
+            event.window().hide().unwrap();
+            api.prevent_close();
+          }
+          _ => {}
+        })
         .invoke_handler(tauri::generate_handler![notify])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
